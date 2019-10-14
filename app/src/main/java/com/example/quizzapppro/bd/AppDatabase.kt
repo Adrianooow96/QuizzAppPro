13c3896a74abec1700b5ecd3cc7dd3b3eeb52f3f
Marco <marcochong.27@gmail.com>
2019-10-13 19:41:30 -0500

    Appdatabase

--- app/src/main/java/com/example/quizzapppro/bd/AppDatabase.kt
+++ app/src/main/java/com/example/quizzapppro/bd/AppDatabase.kt
@@ -1,17 +1,51 @@
 package com.example.quizzapppro.bd
 
 import android.content.Context
-import androidx.room.Database
 import androidx.sqlite.db.SupportSQLiteDatabase
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-/*@Database(
+/*
+@Database(
     entities = [
-        pregunta::class//, Course .class,
+        CourseLevel::class//, Course .class,
         //ProfessorCategory.class, Professor .class
     ], version = 1
-)*/
-
+)
+*/
 abstract class AppDatabase : RoomDatabase() {
+
+
+    companion object {
+
+        private var INSTANCE: AppDatabase? = null
+
+        fun getAppDatabase(context: Context): AppDatabase {
+            if (INSTANCE == null) {
+                INSTANCE = Room.databaseBuilder(
+                    context.getApplicationContext(),
+                    AppDatabase::class.java,
+                    "QuizzDB.db"
+                )
+                    .allowMainThreadQueries()
+                    .addCallback(object : RoomDatabase.Callback() {
+                        override fun onCreate(db: SupportSQLiteDatabase) {
+                            super.onCreate(db)
+                            initializeData(db)
+                        }
+                    })
+                    .build()
+            }
+
+            return INSTANCE as AppDatabase
+        }
+
+        fun initializeData(db: SupportSQLiteDatabase) {
+            //db.beginTransaction();
+
+            db.execSQL("INSERT INTO pregunta (id, description, payment_pct, required_credits) VALUES (0, 'Licenciatura', 100, 120)")
+
+        }
+    }
+
 }
