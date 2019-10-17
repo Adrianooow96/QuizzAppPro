956951a1e04541cabba80c2b9563edea2080199c
Marco <marcochong.27@gmail.com>
2019-10-16 19:55:28 -0500

    ligera correccion

--- app/src/main/java/com/example/quizzapppro/bd/pregunta.kt
+++ app/src/main/java/com/example/quizzapppro/bd/pregunta.kt
@@ -8,7 +8,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "pregunta", indices = [Index(value = ["idPregunta"], unique = true)])
 data class pregunta (
     @PrimaryKey @ColumnInfo(name = "idPregunta") val idPregunta: Int,
-    @field:ColumnInfo(name = "pregunta") val description: String,
+    @field:ColumnInfo(name = "pregunta") val pregunta: String,
     @field:ColumnInfo(name = "categoria") val categoria: Int,
     @field:ColumnInfo(name = "respuestaCorrecta") var respuestaCorrecta: String,
     @field:ColumnInfo(name = "opcion1") var opcion1: String,
