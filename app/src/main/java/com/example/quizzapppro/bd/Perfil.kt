a00c181d783907a40e218edfe1b977e15dc49b33
Marco <marcochong.27@gmail.com>
2019-10-18 19:03:05 -0500

    cosishas de bd

--- app/src/main/java/com/example/quizzapppro/bd/Perfil.kt
+++ app/src/main/java/com/example/quizzapppro/bd/Perfil.kt
@@ -12,7 +12,6 @@ data class Perfil (
     @field:ColumnInfo(name = "avatar") val avatar: Int,
     @field:ColumnInfo(name = "totalPreguntas") var totalPreguntas: Int,
     @field:ColumnInfo(name = "dificultad") var dificultad: Int,
-    @field:ColumnInfo(name = "usarPistas") var usarPistas: Int,
     @field:ColumnInfo(name = "numeroPistas") var numeroPistas: Int,
     @field:ColumnInfo(name = "categoriasElegidas") var categoriasElegidas: String,
     @field:ColumnInfo(name = "status") var status: Int
